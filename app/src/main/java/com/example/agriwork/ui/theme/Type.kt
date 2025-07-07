c2cbf4f3160d7d107b80b13cfcb0f1457d90a6b8
Ankammarao49 <ankammarao49@gmail.com>
2025-07-07 22:53:49 +0530

    rebase

--- app/src/main/java/com/example/agriwork/ui/theme/Type.kt
+++ app/src/main/java/com/example/agriwork/ui/theme/Type.kt
@@ -2,9 +2,20 @@ package com.example.agriwork.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.agriwork.R
+
+val Poppins = FontFamily(
+//    Font(R.font.poppins_light, FontWeight.Light),
+    Font(R.font.poppins_regular, FontWeight.Normal),
+//    Font(R.font.poppins_medium, FontWeight.Medium),
+//    Font(R.font.poppins_semibold, FontWeight.SemiBold),
+//    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_black, FontWeight.Black),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,7 +25,19 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 14.sp
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
